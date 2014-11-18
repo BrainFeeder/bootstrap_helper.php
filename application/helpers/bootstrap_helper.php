064a415cc4f90da466422aa262c95b4fda7e67f6
gp_greg <greg@gridprinciples.com>
2014-11-18 17:45:39 -0500

    Load and use form_helper's _attributes_to_string

--- application/helpers/bootstrap_helper.php
+++ application/helpers/bootstrap_helper.php
@@ -1,9 +1,20 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+// "$this" works under light testing, but experience shows that it won't
+// necessarily work in every context, so let's use a $CI variable
+$CI =& get_instance();
+
+ // we use the CI form helper's _attributes_to_string()
+$CI->load->helper('form');
+
 if(!function_exists('bs_icon'))
 {
 	function bs_icon($sIcon = FALSE)
 	{
-		return '<span class="glyphicon glyphicon-' . $sIcon . '" aria-hidden="true"></span>';
+		$aAttr = array(
+			'class' => 'glyphicon glyphicon-' . $sIcon,
+			'aria-hidden' => 'true',
+		);
+		return '<span' . _attributes_to_string($aAttr) . '></span>';
 	}
 }
