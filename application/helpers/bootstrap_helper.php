c1b82cf4055e6aa7db58ab938592fc220c5a0ad2
gp_greg <greg@gridprinciples.com>
2014-11-18 19:23:14 -0500

    All helper functions now accept an additional parameter of user-defined attributes

--- application/helpers/bootstrap_helper.php
+++ application/helpers/bootstrap_helper.php
@@ -7,26 +7,52 @@ $CI =& get_instance();
  // we use the CI form helper's _attributes_to_string()
 $CI->load->helper('form');
 
+if(!function_exists('_bs_attributes_to_string'))
+{
+	// Merges in a secondary array of user variables,
+	// then uses the CI form helper.
+	function _bs_attributes_to_string($aAttr, $aUserAttr)
+	{
+		$aMergedAttr = $aAttr; // will get sent if $aUserAttr is empty
+
+		if(count($aUserAttr))
+		{
+			// if both arrays sent classes, concatenate them instead of
+			// overwriting one with the other
+			if(isset($aUserAttr['class']) && isset($aAttr['class']))
+			{
+				// Merge both classes into the array with precedence
+				$aUserAttr['class'] = $aAttr['class'].' '.$aUserAttr['class'];
+			}
+			$aMergedAttr = array_merge($aAttr, $aUserAttr); // merge in any passed attributes
+			
+		}
+
+		return _attributes_to_string($aMergedAttr);
+	}
+}
+
 if(!function_exists('bs_icon'))
 {
-	function bs_icon($sIcon = FALSE)
+	function bs_icon($sIcon = FALSE, $aUserAttr = array())
 	{
 		$aAttr = array(
 			'class' => 'glyphicon glyphicon-' . $sIcon,
 			'aria-hidden' => 'true',
 		);
-		return '<span' . _attributes_to_string($aAttr) . '></span>';
+
+		return '<span' . _bs_attributes_to_string($aAttr, $aUserAttr) . '></span>';
 	}
 }
 
 if(!function_exists('bs_container_open'))
 {
-	function bs_container_open($bFluid = FALSE)
+	function bs_container_open($bFluid = FALSE, $aUserAttr = array())
 	{
 		$aAttr = array(
 			'class' => 'container' . ($bFluid ? '-fluid' : ''),
 		);
-		return '<div' . _attributes_to_string($aAttr) . '>';
+		return '<div' . _bs_attributes_to_string($aAttr, $aUserAttr) . '>';
 	}
 }
 
@@ -40,12 +66,12 @@ if(!function_exists('bs_container_close'))
 
 if(!function_exists('bs_row_open'))
 {
-	function bs_row_open()
+	function bs_row_open($aUserAttr = array())
 	{
 		$aAttr = array(
 			'class' => 'row',
 		);
-		return '<div' . _attributes_to_string($aAttr) . '>';
+		return '<div' . _bs_attributes_to_string($aAttr, $aUserAttr) . '>';
 	}
 }
 
@@ -59,7 +85,7 @@ if(!function_exists('bs_row_close'))
 
 if(!function_exists('bs_col_open'))
 {
-	function bs_col_open($mColClasses = '')
+	function bs_col_open($mColClasses = '', $aUserAttr = array())
 	{
 		// split into pieces (space-separated), prepend "col-" to each, and
 		// reattach them to this column's "class"
@@ -83,7 +109,7 @@ if(!function_exists('bs_col_open'))
 		$aAttr = array(
 			'class' => implode(' ', $mColClasses),
 		);
-		return '<div' . _attributes_to_string($aAttr) . '>';
+		return '<div' . _bs_attributes_to_string($aAttr, $aUserAttr) . '>';
 	}
 }
 
@@ -97,7 +123,7 @@ if(!function_exists('bs_col_close'))
 
 if(!function_exists('bs_clearfix'))
 {
-	function bs_clearfix($mColClasses = '')
+	function bs_clearfix($mColClasses = '', $aUserAttr = array())
 	{
 		if(is_string($mColClasses))
 		{			
@@ -125,7 +151,7 @@ if(!function_exists('bs_clearfix'))
 		$aAttr = array(
 			'class' => 'clearfix ' . implode(' ', $mColClasses),
 		);
-		return '<div' . _attributes_to_string($aAttr) . '></div>';
+		return '<div' . _bs_attributes_to_string($aAttr, $aUserAttr) . '></div>';
 	}
 }
 
